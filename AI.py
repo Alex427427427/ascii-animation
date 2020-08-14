835bf5718bb206e32fa90716b93701e72b5c8e09
Alex427427427 <69694377+Alex427427427@users.noreply.github.com>
2020-08-15 05:09:20 +1000

    Add files via upload

---
+++ AI.py
@@ -0,0 +1,39 @@
+# Created by Alexander Li, 14/8/2020
+# To print a series of text files to the terminal, creating a happy birthday message for Incy
+
+import os # used for system calls, namely clear screen
+import time # used for timing - controlling the framerate
+
+
+os.system('cls')
+frame_count = 33 # set number of frames
+file_names = [] # list of files we will use to create the animation
+frames = [] # this will contain a list, each element being a frame. Each frame is a list of lines
+
+
+# add all filenames
+for i in range(frame_count):
+    file_names.append("AI" + str(i + 1) + ".txt")
+
+
+# for each filename
+for name in file_names:
+    # open the files. Don't ask why i use "with". No one understands why.
+    with open(name, "r", encoding="utf8") as f:
+        # add the contents of the file to frames. The content of each file will be a list of utf8 codes in a line.
+        # ASCII would be fine too. But at this stage i'm thinking about using pre-made text files with special
+        # characters. Which may use utf8 instead of plain ASCII.
+        frames.append(f.readlines())
+# SCREW THAT, I AM GOING TO TYPE OUT MY OWN ASCII. I AM GOING TO CREATE EVERYTHING BY MY OWN HANDS AND NO ONE CAN
+# STOP MEEEEEEEEEEEEEEEEEEEE
+# future Alex edit: trying to draw you in ascii did NOT work, holy shit
+
+
+# start animation, repeat 42 times.
+while True:
+    # for each frame...
+    for frame in frames:
+        print("".join(frame)) # join everything into one string
+        time.sleep(0.02) # sleep for 0.02 second
+        os.system('cls') # clear screen
+
---
+++ README.txt
@@ -0,0 +1,13 @@
+Clever, thoughtful, wonderful Incy:
+Happy birthday. May your dearest wishes come true. 
+
+This is for you. 
+Each frame was drawn with a mouse in microsoft paint.
+The files are aptly named "AI" because the source is Alex and the destination is Incy.
+
+PLEASE CLICK MAXIMISE at the command terminal during runtime.
+
+I was inspired by your project. 
+Warning: my commenting in the python files will also burn your eyes.
+You can check out my code if you want, namely the png2ascii.py i wrote to convert png files into ascii text.
+I first tried drawing you in ascii directly and it was... you came out looking like demonic Chara.
