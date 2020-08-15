5b3cfbbdf20471cc2a540f2875ceec45fab0ae22
Alexander Li <69694377+Alex427427427@users.noreply.github.com>
2020-08-15 12:03:54 +1000

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,8 +1,8 @@
 # Created by Alexander Li, 14/8/2020
-# To print a series of text files to the terminal, creating a happy birthday message for Incy
+# To print a series of text files to the terminal, creating a happy birthday message for ****
 
-import os # used for system calls, namely clear screen
-import time # used for timing - controlling the framerate
+import os
+import time
 
 
 os.system('cls')
@@ -20,13 +20,7 @@ for i in range(frame_count):
 for name in file_names:
     # open the files. Don't ask why i use "with". No one understands why.
     with open(name, "r", encoding="utf8") as f:
-        # add the contents of the file to frames. The content of each file will be a list of utf8 codes in a line.
-        # ASCII would be fine too. But at this stage i'm thinking about using pre-made text files with special
-        # characters. Which may use utf8 instead of plain ASCII.
         frames.append(f.readlines())
-# SCREW THAT, I AM GOING TO TYPE OUT MY OWN ASCII. I AM GOING TO CREATE EVERYTHING BY MY OWN HANDS AND NO ONE CAN
-# STOP MEEEEEEEEEEEEEEEEEEEE
-# future Alex edit: trying to draw you in ascii did NOT work, holy shit
 
 
 # start animation, repeat 42 times.
@@ -34,6 +28,6 @@ while True:
     # for each frame...
     for frame in frames:
         print("".join(frame)) # join everything into one string
-        time.sleep(0.2) # sleep for 0.2 second
+        time.sleep(0.1) # sleep for 0.1 second for a 10fps animation
         os.system('cls') # clear screen
 
